5ffa8173ecf9cfed4578292fba44181f981d4006
Thibault Chalaoux <tchalaou@bess-f3r5s5.clusters.42paris.fr>
2025-04-28 15:40:03 +0200

    inception

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,10 +4,12 @@ set -e
 
 WORDPRESS_VERSION=6.4.3
 
-wget https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz -P /tmp
-tar -xzf /tmp/wordpress-${WORDPRESS_VERSION}.tar.gz -C /tmp
-mv /tmp/wordpress/* /var/www/html
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+if [ ! -d /var/www/html/wp-admin ]; then
+    wget https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz -P /tmp
+    tar -xzf /tmp/wordpress-${WORDPRESS_VERSION}.tar.gz -C /tmp
+    mv /tmp/wordpress/* /var/www/html
+    chown -R www-data:www-data /var/www/html
+    chmod -R 755 /var/www/html
+fi
 
 exec php-fpm7.4 -F
